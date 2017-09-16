774847bb6d6efb289d7fbe7e512bb06fc8a7d1f6
Brandon Erbschloe <berbschloe@gmail.com>
2017-09-16 15:01:14 -0500

    Initial Commit

---
+++ Squidward/Squidward.h
@@ -0,0 +1,19 @@
+//
+//  Squidward.h
+//  Squidward
+//
+//  Created by Brandon Erbschloe on 9/16/17.
+//  Copyright © 2017 QuarkWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Squidward.
+FOUNDATION_EXPORT double SquidwardVersionNumber;
+
+//! Project version string for Squidward.
+FOUNDATION_EXPORT const unsigned char SquidwardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Squidward/PublicHeader.h>
+
+
